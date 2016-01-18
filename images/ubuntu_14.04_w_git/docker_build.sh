8ed7ce3cb49fe28a7311e446f617255e5388d411
unai <unai.gg77@gmail.com>
2016-01-18 19:56:18 +0100

    removed git based ubuntu image

--- images/ubuntu_14.04_w_git/docker_build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-HOME_DIR="$( cd "$( dirname "$0" )" && pwd )"
-cp ~/.ssh/id_rsa ${HOME_DIR}/id_rsa
-docker build "$@" -t ubuntu_14.04_w_git ${HOME_DIR}
-rm -rf ${HOME_DIR}/id_rsa
